057b9a3cd36da0ad6a5c6b27a17f90b51916cf43
Ren Delos Reyes <ren.delosreyes@benchling.com>
2021-10-13 14:05:01 -0700

    Fix bucket name

--- root/combine-modules/main.tf
+++ root/combine-modules/main.tf
@@ -2,8 +2,8 @@ terraform {
   required_version = "0.12.31"
   
   backend "s3" {
-    bucket = "test-tf-automation"
-    key    = "benching-security-truss-test-tf-state-us-west-2/terraform.tfstate"
+    bucket = "benching-security-truss-test-tf-state-us-west-2"
+    key    = "tf-automation/terraform.tfstate"
     region = "us-west-2"
     encrypt = true
     dynamodb_table = "terraform-state-lock"
