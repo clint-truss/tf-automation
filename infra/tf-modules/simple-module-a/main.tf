fe06f5871f9bfb18143a648453171355df9191b3
Ren Delos Reyes <ren.delosreyes@benchling.com>
2021-10-14 11:06:07 -0700

    Create a new bucket in simple module a and see if spacelift detects it

--- infra/tf-modules/simple-module-a/main.tf
+++ infra/tf-modules/simple-module-a/main.tf
@@ -8,3 +8,8 @@ resource "aws_s3_bucket" "another_simple_bucket" {
   bucket = "another-simple-bucket"
   acl    = "private"
 }
+
+resource "aws_s3_bucket" "thursday_bucket" {
+  bucket = "thursday-bucket"
+  acl    = "private"
+}
