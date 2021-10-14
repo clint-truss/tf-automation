a79a65f2ee3580727b64f9700fabe9220b03e0ce
pbbench <paul.bort@benchling.com>
2021-10-14 07:50:24 -0700

    Delete root/simple-module-b directory

--- root/simple-module-b/main.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_route53_zone" "benchling_test" {
-  name = "benchling.truss.coffee."
-}
-
-resource "aws_route53_zone" "benchling_test_duo" {
-  name = "benchling.duo.truss.coffee."
-}
