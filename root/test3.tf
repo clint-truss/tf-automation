0eda29bc8509dde6712588df24b01969f1631b2d
pbbench <paul.bort@benchling.com>
2021-10-14 07:26:43 -0700

    Try adding a region?

--- root/test3.tf
+++ root/test3.tf
@@ -1,3 +1,8 @@
+terraform {
+  provider "aws" {
+    region = "us-west-2"
+  }
+}
 
 resource "aws_s3_bucket" "tf_automation_benchling_simple_bucket_un" {
   bucket = "benchling-relatively-simple-bucket-un"
