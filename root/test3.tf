5b4e2f2e56a40534bd8eb619165b935c57b04fd6
pbbench <paul.bort@benchling.com>
2021-10-14 08:20:38 -0700

    Testing Spacelift destroy resource

--- root/test3.tf
+++ root/test3.tf
@@ -7,7 +7,3 @@ resource "aws_s3_bucket" "tf_automation_benchling_simple_bucket_un" {
   acl    = "private"
 }
 
-resource "aws_s3_bucket" "tf_automation_benchling_simple_bucket_deux" {
-  bucket = "benchling-relatively-simple-bucket-deux"
-  acl    = "private"
-}
