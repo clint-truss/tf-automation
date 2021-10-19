e8bba4d91165fd50e6e2d73d4c562b8de36c5b00
Clint Talbert <clint.talbert@benchling.com>
2021-10-19 08:42:44 -0700

    testing tf c take 2

--- infra/root/test3.tf
+++ infra/root/test3.tf
@@ -1,6 +1,6 @@
 
-resource "aws_s3_bucket" "tf_automation_benchling_simple_bucket_duex" {
-  bucket = "benchling-relatively-simple-bucket-duex"
+resource "aws_s3_bucket" "tf_automation_benchling_simple_bucket_duex2" {
+  bucket = "benchling-relatively-simple-bucket-duex2"
   acl    = "private"
 }
 
